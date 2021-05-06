01f32202cba82b60fcbfb6846322cdb022c8e677
menahem <mnjol770@gmail.com>
2021-05-06 17:25:30 +0300

    fix: split router to rautr and controller

--- src/controller/group.controller.ts
+++ src/controller/group.controller.ts
@@ -1,3 +1,93 @@
 import { Request, Response } from "express";
 import personService from "./../service/person.service";
 import groupService from "./../service/group.service";
+
+const createGroup = async (req: Request, res: Response) => {
+  const groupeName = req.params.name;
+  const parentGroup = req.params.parent;
+
+  const newGroup = await groupService.createGroup(groupeName, parentGroup);
+
+  res.send(newGroup);
+};
+
+const changeGroupName = async (req: Request, res: Response) => {
+  const name = req.params.name;
+  const newName = req.params.newName;
+
+  try {
+    const newGroup = await groupService.updateName(name, newName);
+
+    res.send(newGroup);
+  } catch (error) {
+    res.status(400).send("Can't change group");
+  }
+};
+
+const findByField = async (req: Request, res: Response) => {
+  const key = req.params.key;
+  const value = req.params.value;
+
+  const foundedGroup = await groupService.getByField(key, value);
+
+  res.send(foundedGroup || `no group in ${key} => ${value}`);
+};
+
+const allGroups = async (_: Request, res: Response) => {
+  const allGroups = await groupService.getAllGroups();
+  res.send(allGroups);
+};
+
+const addGroupUnderGroup = async (req: any, res: any) => {
+  const to = req.params.insertTo;
+  const child = req.params.toInsert;
+
+  const result = await groupService.connectGroups(to, child);
+
+  res.send(result);
+};
+
+const moveGroupe = async (req: any, res: any) => {
+  const to = req.params.moveTo;
+  const child = req.params.toMove;
+
+  const result = await groupService.moveGroup(to, child);
+
+  res.send(result);
+};
+
+const checkIfPersonInGroup = async (req: Request, res: Response) => {
+  const firstName = req.params.firstName;
+  const groupName = req.params.groupName;
+
+  try {
+    const groupAndPerson = await groupService.isInGroup(firstName, groupName);
+
+    res.send(groupAndPerson);
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+const allHierarchy = async (req: Request, res: Response) => {
+  const groupName = req.params.groupName;
+
+  try {
+    const groupChildren = await groupService.getGroupChildren(groupName);
+
+    res.send(groupChildren || "no groups an people in this group");
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+export default {
+  checkIfPersonInGroup,
+  moveGroupe,
+  addGroupUnderGroup,
+  allGroups,
+  createGroup,
+  changeGroupName,
+  findByField,
+  allHierarchy,
+};
--- src/route/group.route.ts
+++ src/route/group.route.ts
@@ -1,88 +1,24 @@
 import express from "express";
-import groupService from "../service/group.service";
+import groupController from "../controller/group.controller";
 
 const groupRouter = express.Router();
 
-groupRouter.post("/create/:name/parent/:parent", async (req, res) => {
-  const groupeName = req.params.name;
-  const parentGroup = req.params.parent;
-
-  const newGroup = await groupService.createGroup(groupeName, parentGroup);
-
-  res.send(newGroup);
-});
-
-groupRouter.patch("/change/name/:name/toName/:newName", async (req, res) => {
-  const name = req.params.name;
-  const newName = req.params.newName;
-
-  try {
-    const newGroup = await groupService.updateName(name, newName);
-
-    res.send(newGroup);
-  } catch (error) {
-    res.status(400).send("Can't change group");
-  }
-});
-
-groupRouter.get("/fields/:key/:value", async (req, res) => {
-  const key = req.params.key;
-  const value = req.params.value;
-
-  const foundedGroup = await groupService.getByField(key, value);
-
-  res.send(foundedGroup || `no group in ${key} => ${value}`);
-});
-
-groupRouter.get("/all", async (_, res) => {
-  const allGroups = await groupService.getAllGroups();
-  res.send(allGroups);
-});
-
+groupRouter.post("/create/:name/parent/:parent", groupController.createGroup);
+groupRouter.patch(
+  "/change/name/:name/toName/:newName",
+  groupController.changeGroupName
+);
+groupRouter.get("/fields/:key/:value", groupController.findByField);
+groupRouter.get("/all", groupController.allGroups);
 groupRouter.post(
   "/insert/:toInsert/to/:insertTo",
-  async (req: any, res: any) => {
-    const to = req.params.insertTo;
-    const child = req.params.toInsert;
-
-    const result = await groupService.connectGroups(to, child);
-
-    res.send(result);
-  }
+  groupController.addGroupUnderGroup
 );
-
-groupRouter.post("/move/:toMove/to/:moveTo", async (req: any, res: any) => {
-  const to = req.params.moveTo;
-  const child = req.params.toMove;
-
-  const result = await groupService.moveGroup(to, child);
-
-  res.send(result);
-});
-
-groupRouter.get("/:firstName/group/:groupName", async (req, res) => {
-  const firstName = req.params.firstName;
-  const groupName = req.params.groupName;
-
-  try {
-    const groupAndPerson = await groupService.isInGroup(firstName, groupName);
-
-    res.send(groupAndPerson);
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-groupRouter.get("/:groupName/all", async (req, res) => {
-  const groupName = req.params.groupName;
-
-  try {
-    const groupChildren = await groupService.getGroupChildren(groupName);
-
-    res.send(groupChildren || "no groups an people in this group");
-  } catch (error) {
-    res.send(error);
-  }
-});
+groupRouter.post("/move/:toMove/to/:moveTo", groupController.moveGroupe);
+groupRouter.get(
+  "/:firstName/group/:groupName",
+  groupController.checkIfPersonInGroup
+);
+groupRouter.get("/:groupName/all", groupController.allHierarchy);
 
 export default groupRouter;
